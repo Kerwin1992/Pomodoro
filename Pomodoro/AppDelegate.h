62181a933eff80fac591a77f0406876aacb90775
Kerwin1992 <wangcx0905@gmail.com>
2015-11-05 10:51:26 +0800

    Initial Commit

---
+++ Pomodoro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pomodoro
+//
+//  Created by Kerwin on 15/11/5.
+//  Copyright (c) 2015年 Kerwin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pomodoro/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pomodoro
+//
+//  Created by Kerwin on 15/11/5.
+//  Copyright (c) 2015年 Kerwin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
